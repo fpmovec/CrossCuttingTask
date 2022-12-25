ebfc6d7db7ef3dc398b5a75dea720e8f9d56c316
fpmovec <10.maks.10K@gmail.com>
2022-12-25 13:58:11 +0300

    Added Get and Post Tests

---
+++ Cross_Cutting_Task/Cross_Cutting_Task/FileItems/FileItem.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+namespace Cross_Cutting_Task.FileItems;
+
+public class FileItem
+{
+    public int Id { get; set; }
+    public string? InFilePath { get; set; }
+    public string? InArchiveType { get; set; }
+    public string? InFileType { get; set; }
+    public string? OutFileType { get; set; }
+    public string? OutFileName { get; set; }
+    public string? OutArchiveType { get; set; }
+    [JsonProperty("MD5_Result")]
+    public string? EncryptedResult { get; set; }
+    [JsonProperty("Expression")]
+    private string? Expression { get; set; }
+    public Stream? archiveInStream;
+    public Stream? archiveOutStream;
+}
